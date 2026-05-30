c9d5c4cff8636525a8cbc8d4375f6788544ddafd
Aditi <aditiprajapati601@gmail.com>
2026-05-30 12:42:33 +0530

    Restore original content in movie.py

--- Folder 2/movie.py
+++ Folder 2/movie.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv(r"task2\IMDb Movies India.csv", encoding='latin1')
+df = pd.read_csv("task2\IMDb Movies India.csv", encoding='latin1')
 
 print(df.head()
 ,df.shape
